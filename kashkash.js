4e28eb6f5c225b3956c6022803d995f2129aa0a3
youse22 <youseniedorsainvil@gmail.com>
2024-01-31 01:49:20 -0500

    first commit

---
+++ kashkash.js
@@ -0,0 +1,8 @@
+  
+const sekre0=document.getElementById('wrapper')
+let sek = sekre0.querySelector('div:nth-child(3) > span').textContent="kachèt revele"
+
+const span=document.getElementsByClassName("div-wrapper")[0]
+span.getElementsByTagName('span').item(1).innerHTML="kachèt revele"
+
+const sekre=document.querySelectorAll('div,#wrapper,span').item(1).innerText="kachèt revele"
